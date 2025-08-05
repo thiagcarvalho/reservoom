0094c91f728307de0e858e2de92e3ea67d777e8f
Thiago Carvalho <thiagoc413@gmail.com>
2025-08-05 15:04:20 -0300

    add migrations to the program

---
+++ Reservoom/DTOs/ReservationDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.DTOs
+{
+    public class ReservationDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public int FloorNumber { get; set; }
+        public int RoomNumber { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ Reservoom/DbContexts/ReservoomDesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using SQLitePCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.DbContexts
+{
+    public class ReservoomDesignTimeDbContextFactory : IDesignTimeDbContextFactory<ReservoomDbContext>
+    {
+        public ReservoomDbContext CreateDbContext(string[] args)
+        {
+            Batteries_V2.Init();
+            DbContextOptions options = new DbContextOptionsBuilder().UseSqlite("Data Source=reservoom.db").Options;
+
+            return new ReservoomDbContext(options);
+        }
+    }
+}
